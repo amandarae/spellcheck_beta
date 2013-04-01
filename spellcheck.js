1ca6c3f38455558b1688e18a45f49e68178ce5b5
amanda rae <amanda@amandarae.ca>
2013-04-01 18:34:28 -0400

    2nd build
    recursively iterate through dom elements of specified div

--- spellcheck.js
+++ spellcheck.js
@@ -1,84 +1,80 @@
-//Array sanitizing function
-Array.prototype.sanitize = function(sanitizeValue){
-    for(var i = 0;i < this.length; i++){
-    if(this[i]==sanitizeValue){
-        //remove empty result from array
-        this.splice(i,1);
-        //adjust count of array
-        i--;
+function spellCheck(divID, isInput){
+    var out = new Array();
+    var start = document.getElementById(divID);
+    if(!isInput)
+      var output = start.innerHTML;
+
+    outputErrors(traverse());
+
+  function traverse(element){
+    var element = element || start; 
+    if(element.hasChildNodes()){
+      var child = element.firstChild;
+      while(child){
+        if(child.nodeType === 1 || child.nodeType === 3)
+          traverse(child)
+        child = child.nextSibling;
+      }
     }
+    if(element.data){
+      var str = element.data;
+      str = stripNewLines(str);
+      str = stripExcessWhite(str);
+      str = stripPunctuation(str);
+      if(str && str !== " "){
+          str = str.split(" ");
+          if(str.length > 1)
+              sanitize(str, "");
+          for(var i=0;i<str.length;i++){
+              var correct = stringCompare(str[i]);
+              if(!correct)
+                out.push(str[i]);
+           }
+      }
     }
-    return this;
-};
+  return out;
+  }
 
-//String sanitizing functions
-String.prototype.stripTags = function(){
-     return this.replace(/<[^>]+>/g," ");
-};
-
-String.prototype.stripNewLines = function(){
-    return this.replace(/\r\n|\n|\r/gm," ");
-};
-
-String.prototype.stripExcessWhite = function(){
-    return this.replace(/s{2,}/gm,"");
-};
-
-String.prototype.stripPunctuation = function(){
-    return this.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/gm," ");
-};
-
-//prep input
-function prepInput(string){
-  var x = string.stripTags().stripPunctuation().stripNewLines().stripExcessWhite().split(" ");
-  x.sanitize("");
-  return x;
+  function outputErrors(array){
+   for(var e=0; e<array.length;e++){
+    var reg = new RegExp(array[e],"g");
+    output = output.replace(reg, "<span style='color:red'>"+array[e]+"</span>");
+   }
+    start.innerHTML = output;
+  }
 }
 
-//compare input strings to dictionary strings
-function stringCompare(dictionary, input){
+
+function stripNewLines(input){
+   return input.replace(/\r\n|\n|\r/gm, '');
+};
+
+function stripExcessWhite(input){
+    return input.replace(/ {2,}?/gm,'');
+};
+
+function stripPunctuation(input){
+    return input.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/gm,' ');
+};
+
+function sanitize(array, sanitizeValue){
+    for(var i = 0;i < array.length; i++){
+        if(array[i]==sanitizeValue){
+            array.splice(i,1);
+            i--;
+        }
+    }
+    return array;
+};
+
+function stringCompare(input){
     var valid = false;
-    for(var i=0;i<input.length;i++){
-        for(var d=0;d<dictionary.length;d++){
-            if(input[i] === dictionary[d]){
-               valid = true;
-               input.splice(i,1);
-               i--;
-               break;
-            }
+    var dictionary=["The", "quick", "brown", "fox", "jumped", "over", "the", "fence", "and", "went", "through", "yard", "When", "Billy", "went", "to", "school", "he", "became", "smarter", "everyday", "Jordan", "worked", "at", "became", "a", "super", "star", "He", "made", "lots", "of", "cool", "applications", "One", "his", "best", "applications", "was", "one", "that", "could", "predict", "future", "Please", "tell", "us", "your", "name", "Name", "Thank", "you"];
+    for(var d=0;d<dictionary.length;d++){
+        if(input === dictionary[d]){
+            valid = true;
+            break;
         }
-        if(valid){valid = false;}
     }
-    return input;
-}
-
-//prep input, compare strings, output input with highlighted failures
-function spellCheck(divID, dictionary, isInput){
-    var str, input;
-    if(!isInput){
-        //non-input
-        str = document.getElementById(divID).innerHTML;
-        input = prepInput(str);
-        input = stringCompare(dictionary, input);
-        if(input.length < 1)
-          return false;
-         for(var e=0; e<input.length;e++){
-            var reg = new RegExp(input[e],"g");
-            str = str.replace(reg, "<span style='color:red'>"+input[e]+"</span>");
-         }
-         return str;
-    }
-    
-    if(isInput){
-        //inputs
-        str = document.getElementById(divID).value;
-        input = prepInput(str);
-        input = stringCompare(dictionary, input);
-        if(input.length < 1){
-          document.getElementById(divID).style.border='';
-           return false;
-        }
-        document.getElementById(divID).style.border='1px solid red';
-        return input;
-    }
+    return valid;
 }
